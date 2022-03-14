864e5c1ff914823bbe7cad4e4094475d6cb873fe
Agustinf90 <agustinf_tw@hotmail.com>
2022-03-14 19:04:15 -0300

    intro express

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import momento from 'moment';
+import moment from 'moment';
 import fs from 'fs'
 
 class Archivo {
@@ -7,15 +7,6 @@ class Archivo {
     productos = [{title: 'Fideos', price: 200, thumbnail: 'dsgasdgasdg', id: 1}, {title: 'Agua', price: 400, thumbnail: 'sdgqasgadsa', id: 2}];
 
 
-    guardar = async (title, price, thumbnail) => {
-
-        try {
-            this.productos.push({title , price, thumbnail: thumbnail, id: this.productos.length+1})
-            await fs.promises.writeFile('productos.txt', JSON.stringify(this.productos))
-        } catch (error) {
-            console.log("Can't process file: " + error)
-        }
-    }
     leer = async () => {
 
         try {
@@ -27,23 +18,14 @@ class Archivo {
             return console.log("Can't process file: " + error)
         }
     }
-    eliminar = async () => {
-        try {
-            await fs.promises.unlink('productos.txt')
-            return console.log('Archivo Eliminado')
 
-        }
-        catch {
-            return console.log("Can't process file: " + error)
-        }
-    }
 }
 
 const productosFile = new Archivo()
 const items = JSON.parse(await productosFile.leer())
 
 const itemsRandom = items[Math.floor(Math.random() * items.length)]
-let visits = 0
+
 
 const app = express()
 const PORT = 8080;
@@ -52,22 +34,27 @@ const server = app.listen(PORT, () => {
     console.log(`listening to port ${PORT}`)
 })
 
+let visitsEndpoint1 = 0
+let visitsEndpoint2 = 0
+
+let json = [{'endpoint1': visitsEndpoint1},{'endpoint2': visitsEndpoint2}]
+
 app.get('/items', async (req, res) => {
-    let visitsItems = visits++
+    visitsEndpoint1++
     
+    json[0].endpoint1 = visitsEndpoint1
+
     res.send({items, cantidad: items.length})
 })
-console.log(visits)
 
 app.get('/items-random', async (req, res) =>{
-    let visitsItemsRandom = visits++
-    res.send({item: itemsRandom})
 
+    visitsEndpoint2++
+    json[1].endpoint2 = visitsEndpoint2
+
+    res.send({item: itemsRandom})
 })
+
 app.get('/visitas', async (req,res)=> {
-    res.send({ visitsItems , visitsItemsRandom})
+    res.send({visitas: json})
 })
-// getBookNames(){
-//     let nombreslibros = this.libros.map(libro=>{
-//         return  libro.book;
-//     })
